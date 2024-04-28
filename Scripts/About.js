0b7ee44f68cd04f7ebedb76a048fbb554eba1a23
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-04-28 17:37:50 +0300

    Update About.js

--- Scripts/About.js
+++ Scripts/About.js
@@ -1,7 +1,7 @@
 function aboutPopUp() {
   let body = document.getElementsByClassName('Container')[0];
   let popUpHeader = "<div class='PopUpHeader'><h1 class='PopUpText'>About</h1></div>";
-  let popUpBodyText = "<h1 class='PopUpBodyText'>Developed by<br>Ali BEYAZ<br>for<br>İlknur BEYAZ<br>with ❤<br>2024/04/28</h1>";
+  let popUpBodyText = "<h1 class='PopUpBodyText'>Developed by<br>Ali BEYAZ<br>for<br>İlknur BEYAZ<br>with ❤<br>v1.0.0 - 2024/04/28</h1>";
   let br = "<br>";
 
   let buttonsContainer = "<div id='buttonsContainer'><button id='closeButton' class='generalButton' style='cursor: pointer;'>Close</button></div>";
