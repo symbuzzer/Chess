56ab3a13371f4696142e3a0bc4757b20e93687b0
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-04-28 15:42:20 +0300

    Update About.js

--- Scripts/About.js
+++ Scripts/About.js
@@ -1,13 +1,23 @@
 function aboutPopUp(){
     let body = document.getElementsByClassName('Container')[0];
     
-    let popUpHeader = "<div class='PopUpHeader'> <h1 class='PopUpText'>About:</h1>    </div>";
-    let popUpBodyText = "<h1 class='PopUpBodyText'>Developed by Ali BEYAZ for İlknur BEYAZ with ❤</h1>";
-
+    let popUpHeader = "<div class='PopUpHeader'> <h1 class='PopUpText'>About</h1></div>";
+    let popUpBodyText = "<h1 class='PopUpBodyText'>Developed by<br>Ali BEYAZ<br>for<br>İlknur BEYAZ ❤</h1>";
     let br = "<br>";
 
-    let popUpBody = "<div class='PopUpBody'>" + popUpBodyText +br + br + "</div>";
+    let closeButton = "<button id='closeButton' style='cursor: pointer;'>Close</button>";  // Close butonu ekleme
 
-    let popUp = "<div id='PopUp'>"+ popUpHeader + popUpBody +"</div>";
+    let popUpBody = "<div class='PopUpBody'>" + popUpBodyText + br + br + closeButton + "</div>";  // Butonu pop-up body'e ekleme
+
+    let popUp = "<div id='PopUp'>" + popUpHeader + popUpBody + "</div>";
     body.innerHTML += popUp;
+
+    document.getElementById('closeButton').addEventListener('click', closePopUp);
+}
+
+function closePopUp() {
+    let popUp = document.getElementById('PopUp');
+    if (popUp) {
+        popUp.parentNode.removeChild(popUp);
+    }
 }
