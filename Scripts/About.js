7e862c849e8fe2860e1366cde542b68156af3dfb
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-04-28 15:50:50 +0300

    Update About.js

--- Scripts/About.js
+++ Scripts/About.js
@@ -1,16 +1,22 @@
-function aboutPopUp(){
+function aboutPopUp() {
     let body = document.getElementsByClassName('Container')[0];
     
-    let popUpHeader = "<div class='PopUpHeader'> <h1 class='PopUpText'>About</h1></div>";
-    let popUpBodyText = "<h1 class='PopUpBodyText'>Developed by<br>Ali BEYAZ<br>for<br>İlknur BEYAZ ❤</h1>";
+    let popUpHeader = "<div class='PopUpHeader'><h1 class='PopUpText'>About:</h1></div>";
+    let popUpBodyText = "<h1 class='PopUpBodyText'>Developed by Ali BEYAZ for İlknur BEYAZ with ❤</h1>";
     let br = "<br>";
 
-    let closeButton = "<button id='closeButton' style='cursor: pointer;'>Close</button>";  // Close butonu ekleme
+    // buttonsContainer ve restartButton div'lerini oluşturma
+    let buttonsContainer = "<div id='buttonsContainer'>" +
+        "<div id='restartButton'></div>" +
+        "</div>";
 
-    let popUpBody = "<div class='PopUpBody'>" + popUpBodyText + br + br + closeButton + "</div>";  // Butonu pop-up body'e ekleme
-
+    let popUpBody = "<div class='PopUpBody'>" + popUpBodyText + br + br + buttonsContainer + "</div>";
+    
     let popUp = "<div id='PopUp'>" + popUpHeader + popUpBody + "</div>";
     body.innerHTML += popUp;
+
+    let closeButton = "<button id='closeButton' style='cursor: pointer;'>Close</button>";
+    document.getElementById('generalButton').innerHTML = closeButton;
 
     document.getElementById('closeButton').addEventListener('click', closePopUp);
 }
