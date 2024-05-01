46b82b97d2607769059cf4e7a47a01ab1d4dc1f4
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-05-01 18:45:14 +0300

    Delete Scripts/Theme.js

--- Scripts/Theme.js
+++
@@ -1,9 +0,0 @@
-  function updateTheme() {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.body.style.backgroundColor = 'black';
-      document.body.style.color = 'white';
-    } else {
-      document.body.style.backgroundColor = 'white';
-      document.body.style.color = 'black';
-    }
-  }
