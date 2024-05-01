f90982da6599bceeb1ba5ab68e3ec4345311cf9f
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-05-01 17:40:37 +0300

    Create About.js

---
+++ Scripts/About.js
@@ -0,0 +1,14 @@
+let popUp = document.getElementById("PopUp");
+
+function makePopUp(event){
+    let body = document.getElementsByClassName('Container')[0];
+    
+    let popUpHeader = "<div class='PopUpHeader'> <h1 class='PopUpText'>"+ "About" +"</h1>    </div>";
+
+    let popUpAboutText = "<h3 class='PopUpBodyText'><br><br>from ALİ to İLKNUR with ❤<br><br>v1.0.2 - 2024/05/01</h3>";
+
+    let popUpBody = "<div class='PopUpBody'>" + popUpAboutText + "</div>";
+
+    let popUp = "<div id='PopUp'>"+ popUpHeader + popUpBody +"</div>";
+    body.innerHTML += popUp;
+}
